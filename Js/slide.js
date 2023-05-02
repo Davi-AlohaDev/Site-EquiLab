b7d3d9aa7a5d995c16b2c42a3179e63fc94e4f16
Aloha-dev-D <127554027+Davi-AlohaDev@users.noreply.github.com>
2023-05-02 14:01:18 -0400

    Initial commit

---
+++ Js/slide.js
@@ -0,0 +1,18 @@
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+
+
+document.getElementById('radio1').checked =  true 
+
+setInterval(() => {
+    proxImg()
+}, 6000)
+
+function proxImg(){
+    cont ++
+
+    if(cont > 4){
+        cont = 1
+    }
+    document.getElementById('radio'+cont).checked = true 
+}
